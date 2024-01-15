5d2e6ea4c36513200ebc28c025e8ae7360386d32
Miguel Guevara <miguel_g1789@hotmail.com>
2024-01-15 18:55:11 -0500

    parallax hecho

--- src/components/ParallaxSection/ParallaxSection.js
+++ src/components/ParallaxSection/ParallaxSection.js
@@ -1,23 +1,29 @@
-import parallaxSection from "@/data/parallaxSection";
 import dynamic from "next/dynamic";
 import React from "react";
 import JarallaxImage from "../Jarallax/JarallaxImage";
 const Jarallax = dynamic(() => import("../Jarallax/Jarallax"), { ssr: false });
 
-const { bg, icon, title, title2 } = parallaxSection;
-
 const ParallaxSection = () => {
   return (
     <Jarallax speed={0.3} imgPosition="50% 80%" className="parallax-section">
-      <JarallaxImage src={bg.src} />
+      <JarallaxImage
+        src={require("../../assets/images/integralSecurity.png").default.src}
+      />
       <div className="auto-container">
         <div className="content-box">
-          <div className="icon-box">
-            <span className={icon}></span>
-          </div>
           <h2>
-            {title} <span>{title2}</span>
+            SEGURIDAD <span>INTEGRAL</span>
           </h2>
+          <p>
+            Nuestra Solución provee la{" "}
+            <span style={{ fontWeight: "bolder" }}>TRAZABILIDAD</span> necesaria
+            para las operaciones portuarias brindando{" "}
+            <span style={{ fontWeight: "bolder" }}>INFORMACION ONLINE</span> que
+            permite <span style={{ fontWeight: "bolder" }}>AGILIZAR</span> los
+            procesos de inspección{" "}
+            <span style={{ fontWeight: "bolder" }}>INCREMENTANDO</span> la
+            seguridad de la carga
+          </p>
         </div>
       </div>
     </Jarallax>
